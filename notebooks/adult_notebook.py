49cfdf224e6a7914ffd3f59f3b5e4304e0fdb542
Mohammed Abid <kamranmohammedabid@gmail.com>
2026-03-26 16:33:20 -0700

    Adding files

---
+++ notebooks/adult_notebook.py
@@ -0,0 +1,6 @@
+import pandas as pd
+cvs = pd.read_csv("/Users/kamranabid/Downloads/adult.csv")
+
+# Which combination of 'education' and 'marital.status'
+# has the highest percentage of individuals earning more than 50K?
+print(cvs)
